1fb759dbc67111b9d40ce6b66be093a1df125780
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2024-02-28 06:54:47 +0600

    string type variable

---
+++ variableDataTypes.js
@@ -0,0 +1,11 @@
+// String type variable
+var name1 = 'tanvir';
+console.log(name1);
+// ans :-  tanvir
+
+var name2 = "hasan"; //it can be single quotation or double quotation
+console.log(name2);
+// ans :-  hasan
+
+
+// Number type variable
