4c8d31422ab84be1db1a3c8084f579be70f5e3d0
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2024-02-28 06:45:37 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,12 @@
+var weight = 38;
+console.log(weight)
+// ans :- 38
+
+var money = 5000;
+console.log(money)
+// ans :- 5000
+
+var age = 12;
+age = 40;   // variable's value is changeable
+console.log(age)
+// ans :- 40
